5c0141d11610eb9e16e89ae8fccdb5fd6a661a00
ryanzg123 <ganzhiheng1@gmail.com>
2023-08-31 02:55:02 -0400

    update next config to use external image source from discord

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.discordapp.com',
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
