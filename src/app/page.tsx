89a976e3ecac5927b6267fb5291be1d9c9955508
ryanzg123 <ganzhiheng1@gmail.com>
2023-08-31 13:09:53 -0400

    setup intro section (1st) section of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Intro from '@/components/intro';
+
 export default function Home() {
-    return <main className=""></main>;
+    return (
+        <main className="flex flex-col items-center px-4">
+            <Intro />
+        </main>
+    );
 }
---
+++ src/components/intro.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight } from 'react-icons/bs';
+
+export default function Intro() {
+    return (
+        <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+            <div className="flex items-center justify-center">
+                <div className="relative">
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            type: 'tween',
+                            duration: 0.25,
+                            delay: 0.5,
+                        }}
+                    >
+                        <Image
+                            src="https://cdn.discordapp.com/attachments/1095855762103865454/1095855782551105556/IMG_4427.JPG"
+                            alt="Ryan Gan"
+                            width={200}
+                            height={200}
+                            quality={95}
+                            priority={true}
+                            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+                        />
+                    </motion.div>
+
+                    <motion.span
+                        className="absolute bottom-0 right-0 text-4xl"
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            type: 'spring',
+                            stiffness: 125,
+                            delay: 0.75,
+                            duration: 0.7,
+                        }}
+                    >
+                        👋
+                    </motion.span>
+                </div>
+            </div>
+
+            <motion.h1
+                className="mb-10 mt-4 px-4 text-2xl leading-[1.5] sm:text-4xl"
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                    type: 'tween',
+                    duration: 0.5,
+                    delay: 1,
+                }}
+            >
+                <span className="font-bold">Hello, I&apos;m Ryan Gan. </span>
+                I&apos;m a
+                <span className="font-bold"> full-stack developer</span>. I
+                enjoy building
+                <span className="italic"> web & mobile applications</span>.
+            </motion.h1>
+
+            <div>
+                <Link
+                    href="contact"
+                    className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                >
+                    Contact me
+                    <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+                </Link>
+            </div>
+        </section>
+    );
+}
