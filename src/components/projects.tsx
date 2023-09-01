878280c693e5d31833adda84246e4187b4e0b9d9
ryanzg123 <ganzhiheng1@gmail.com>
2023-09-01 17:47:22 -0400

    set up project section

---
+++ src/components/projects.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SectionHeader from './section-header';
+import { projectsData } from '@/lib/data';
+import Project from './project';
+
+export default function Projects() {
+    return (
+        <section id="projects" className="scroll-mt-28 mb-28 sm:mb-40">
+            <SectionHeader>My Projects</SectionHeader>
+            <div>
+                {projectsData.map((project, index) => (
+                    <Project key={index} {...project} />
+                ))}
+            </div>
+        </section>
+    );
+}
