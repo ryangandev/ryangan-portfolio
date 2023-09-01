9fd2707de1e1e9d812d569891dd5d551aacb2067
ryanzg123 <ganzhiheng1@gmail.com>
2023-09-01 14:09:48 -0400

    complete about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import About from '@/components/about';
 import Intro from '@/components/intro';
 import SectionDivider from '@/components/section-divider';
 
@@ -6,6 +7,7 @@ export default function Home() {
         <main className="flex flex-col items-center px-4">
             <Intro />
             <SectionDivider />
+            <About />
         </main>
     );
 }
