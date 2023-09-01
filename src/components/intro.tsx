f0627a0d88b586c3e38602685549ce28ea2278f0
ryanzg123 <ganzhiheng1@gmail.com>
2023-09-01 02:22:49 -0400

    update intro section with buttons and icons

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { BsArrowRight } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { LinkedInIcon, GitHubIcon, FacebookIcon } from '@/assets/icons/index';
 
 export default function Intro() {
     return (
@@ -64,7 +66,14 @@ export default function Intro() {
                 <span className="italic"> web & mobile applications</span>.
             </motion.h1>
 
-            <div>
+            <motion.div
+                className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                    delay: 0.1,
+                }}
+            >
                 <Link
                     href="contact"
                     className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
@@ -72,7 +81,39 @@ export default function Intro() {
                     Contact me
                     <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
                 </Link>
-            </div>
+                <a
+                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                    href="/CV.pdf"
+                    download
+                >
+                    Download CV
+                    <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+                </a>
+
+                <a
+                    className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                    href="https://linkedin.com"
+                    target="_blank"
+                >
+                    <LinkedInIcon />
+                </a>
+
+                <a
+                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                    href="https://github.com"
+                    target="_blank"
+                >
+                    <GitHubIcon />
+                </a>
+
+                <a
+                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                    href="https://facebook.com"
+                    target="_blank"
+                >
+                    <FacebookIcon />
+                </a>
+            </motion.div>
         </section>
     );
 }
