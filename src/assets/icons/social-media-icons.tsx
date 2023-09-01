5b0446b56edb3fb58264f2ba8dcad526230d3fe1
ryanzg123 <ganzhiheng1@gmail.com>
2023-09-01 02:22:26 -0400

    set up icon assets

---
+++ src/assets/icons/social-media-icons.tsx
@@ -0,0 +1,15 @@
+import { BsLinkedin, BsGithub, BsFacebook } from 'react-icons/bs';
+
+const LinkedInIcon = () => {
+    return <BsLinkedin style={{ color: '#007bb5' }} />;
+};
+
+const GitHubIcon = () => {
+    return <BsGithub style={{ color: 'black' }} />;
+};
+
+const FacebookIcon = () => {
+    return <BsFacebook style={{ color: '#4267B2' }} />;
+};
+
+export { LinkedInIcon, GitHubIcon, FacebookIcon };
