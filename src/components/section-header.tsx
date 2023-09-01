4fda9fa76b07ab9f925dbd9d3dcbf1a59d3d5c67
ryanzg123 <ganzhiheng1@gmail.com>
2023-09-01 17:24:15 -0400

    center section header

--- src/components/section-header.tsx
+++ src/components/section-header.tsx
@@ -5,5 +5,9 @@ interface SectionHeaderProps {
 }
 
 export default function SectionHeader({ children }: SectionHeaderProps) {
-    return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>;
+    return (
+        <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+            {children}
+        </h2>
+    );
 }
