9a1779ef3c44977ee7566f4aa91fb5ce9defd659
ryanzg123 <ganzhiheng1@gmail.com>
2023-09-01 02:22:05 -0400

    animated motion divider complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from '@/components/intro';
+import SectionDivider from '@/components/section-divider';
 
 export default function Home() {
     return (
         <main className="flex flex-col items-center px-4">
             <Intro />
+            <SectionDivider />
         </main>
     );
 }
