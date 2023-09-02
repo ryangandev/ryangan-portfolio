af104823a206bb059f0bbf6576c70e307da5ef10
ryanzg123 <ganzhiheng1@gmail.com>
2023-09-01 22:16:47 -0400

    create a icon model for type check

---
+++ src/models/icon.ts
@@ -0,0 +1,42 @@
+enum SocialMedia {
+    LinkedIn = 'LinkedIn',
+    GitHub = 'GitHub',
+    Facebook = 'Facebook',
+}
+
+enum TechStack {
+    // Frontend
+    React = 'React',
+    TypeScript = 'TypeScript',
+    JavaScript = 'JavaScript',
+    HTML5 = 'HTML5',
+    CSS3 = 'CSS3',
+    NextJS = 'NextJS',
+    Redux = 'Redux',
+    TailwindCSS = 'TailwindCSS',
+    Expo = 'Expo',
+    ReactNative = 'ReactNative',
+    Swift = 'Swift',
+    Unity = 'Unity',
+
+    // Backend
+    NodeJS = 'NodeJS',
+    SpringBoot = 'SpringBoot',
+    SocketIO = 'SocketIO',
+
+    // Database
+    SQLite = 'SQLite',
+    PostgreSQL = 'PostgreSQL',
+
+    // UI/UX
+    Figma = 'Figma',
+    AdobePs = 'AdobePs',
+
+    // Deployment
+    Vercel = 'Vercel',
+    AWS = 'AWS',
+    Akamai = 'Akamai',
+    Docker = 'Docker',
+}
+
+export { SocialMedia, TechStack };
