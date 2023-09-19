335839bd2864f79a9b53716d58f5acb057b0a4ce
Ryan Gan <ganzhiheng1@gmail.com>
2023-09-18 22:11:19 -0400

    apply custom hook useActiveSection to animate nav bar when clicked on section

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,15 @@
 'use client';
 
-import React from 'react';
 import { motion } from 'framer-motion';
 import { links } from '@/lib/data';
 import Link from 'next/link';
+import clsx from 'clsx';
+import { useActiveSection } from '@/hooks/useActiveSection';
 
-export default function Header() {
+export default function NavBar() {
+    const { activeSection, setActiveSection, setTimeOfLastClick } =
+        useActiveSection();
+
     return (
         <header className="z-[999] relative">
             <motion.div
@@ -25,10 +29,31 @@ export default function Header() {
                             animate={{ y: 0, opacity: 1 }}
                         >
                             <Link
-                                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
+                                className={clsx(
+                                    'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300',
+                                    {
+                                        'text-gray-950 dark:text-gray-200':
+                                            activeSection === link.name,
+                                    },
+                                )}
                                 href={link.hash}
+                                onClick={() => {
+                                    setActiveSection(link.name);
+                                }}
                             >
                                 {link.name}
+
+                                {link.name === activeSection && (
+                                    <motion.span
+                                        className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                                        layoutId="activeSection"
+                                        transition={{
+                                            type: 'spring',
+                                            stiffness: 380,
+                                            damping: 30,
+                                        }}
+                                    ></motion.span>
+                                )}
                             </Link>
                         </motion.li>
                     ))}
