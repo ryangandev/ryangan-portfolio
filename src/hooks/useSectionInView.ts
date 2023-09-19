47f50c8ec7caadf4df803babf3403abe30e9ced6
Ryan Gan <ganzhiheng1@gmail.com>
2023-09-18 22:09:08 -0400

    a custom hook that animates the nav bar when scrolling into each section

---
+++ src/hooks/useSectionInView.ts
@@ -0,0 +1,22 @@
+import { useActiveSection } from '@/hooks/useActiveSection';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import type { SectionName } from '@/models/section';
+
+const useSectionInView = (sectionName: SectionName, threshold = 0.75) => {
+    const { ref, inView } = useInView({
+        threshold, // threshold means the percentage of the section that must be visible to trigger the inView callback
+    });
+    const { setActiveSection, timeOfLastClick } = useActiveSection();
+
+    // To prevent intersection observer from firing on sections that are not the section clicked by the user
+    useEffect(() => {
+        if (inView && Date.now() - timeOfLastClick > 1000) {
+            setActiveSection(sectionName);
+        }
+    }, [inView, sectionName, setActiveSection, timeOfLastClick]);
+
+    return { ref };
+};
+
+export { useSectionInView };
