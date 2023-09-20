122bf25c64651484164ed6d8eeb02fd67ca7fa27
Ryan Gan <ganzhiheng1@gmail.com>
2023-09-19 23:00:04 -0400

    update active dark style

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -32,7 +32,7 @@ export default function NavBar() {
                                 className={clsx(
                                     'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300',
                                     {
-                                        'text-gray-950 dark:text-gray-200':
+                                        'text-gray-950 dark:text-gray-300':
                                             activeSection === link.name,
                                     },
                                 )}
