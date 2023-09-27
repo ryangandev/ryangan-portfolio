9fc018be0c83070eeb4d5732bfc5ffd06eb52070
Ryan Gan <ganzhiheng1@gmail.com>
2023-09-26 21:27:33 -0400

    Organized projects components

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -5,6 +5,8 @@ import Image, { StaticImageData } from 'next/image';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { TechStackIcon } from '@/assets/icons';
 import { TechStack } from '@/models/data';
+import { useDisclosure } from '@nextui-org/react';
+import ProjectModal from './project-modal';
 
 interface ProjectProps {
     projectTitle: string;
@@ -29,6 +31,7 @@ export default function Project({
     });
     const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
     const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
     return (
         <motion.div
@@ -38,6 +41,7 @@ export default function Project({
                 opacity: opacityProgress,
             }}
             className="group mb-3 sm:mb-8 last:mb-0"
+            onClick={onOpen}
         >
             <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 sm:group-even:pr-0 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
                 <div className="pt-4 pb-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:pb-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
@@ -72,6 +76,7 @@ export default function Project({
                                 group-even:right-[initial] group-even:-left-40"
                 />
             </section>
+            <ProjectModal isOpen={isOpen} onOpenChange={onOpenChange} />
         </motion.div>
     );
 }
