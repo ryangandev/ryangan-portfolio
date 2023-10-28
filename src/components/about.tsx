baa483203493b5ee82c4c144c5d8e1695c72b7e7
ryanzg123 <ganzhiheng1@gmail.com>
2023-10-28 16:12:38 -0400

    Removed old text

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -18,76 +18,7 @@ export default function About() {
             id="about"
         >
             <SectionHeader>About me</SectionHeader>
-            {/* <p className="mb-3">
-                I am currently a senior at Drexel University, pursuing a{' '}
-                <span className="font-medium">Bachelor&apos;s degree</span> in{' '}
-                <span className="font-medium">Computer Science</span> with
-                concentrations in{' '}
-                <span className="font-medium">Artificial Intelligence</span> and{' '}
-                <span className="font-medium">Game Development</span>. My
-                journey began with a passion for programming and has since
-                evolved into a love for{' '}
-                <span className="font-medium">full-stack development</span>.
-            </p>
 
-            <p className="mb-3">
-                For me,{' '}
-                <span className="italic">software development is an art</span>.
-                It&apos;s about crafting{' '}
-                <span className="italic">intuitive solutions</span> that
-                aren&apos;t just functional but also offer users a delightful
-                experience. What I relish most about programming is the{' '}
-                <span className="font-medium">
-                    challenge of problem-solving
-                </span>
-                . Crafting efficient, clean code is a joy, not just for the
-                performance benefits but also because it enhances{' '}
-                <span className="font-medium">
-                    readability and comprehension
-                </span>
-                .
-            </p>
-
-            <p className="mb-3">
-                My expertise spans a{' '}
-                <span className="font-medium">broad tech stack</span>, including
-                frontend tools like{' '}
-                <span className="font-medium">
-                    React, React Native, and Next.Js
-                </span>
-                , backend frameworks such as{' '}
-                <span className="font-medium">Node.js and Spring Boot</span>,
-                and databases like{' '}
-                <span className="font-medium">Postgres and MySQL</span>. As I
-                seek a{' '}
-                <span className="italic">
-                    full-time software developer role
-                </span>
-                , I&apos;m also expanding my skills, currently exploring{' '}
-                <span className="font-medium">
-                    iOS development with SwiftUI{' '}
-                </span>
-                and the <span className="font-medium">Rust</span> programming
-                language.
-            </p>
-
-            <p className="mb-3">
-                I carry with me not just my technical expertise but also a{' '}
-                <span className="font-medium">growth mindset</span>. I believe
-                in the power of{' '}
-                <span className="italic">incremental learning</span>. Even a
-                small nugget of new knowledge each day can be transformative,
-                sculpting me into a better professional and individual.
-            </p>
-
-            <p>
-                Away from the coding, I unwind with video games and spending
-                time with my cats.{' '}
-                <span className="italic">
-                    Everyday is an opportunity to learn
-                </span>
-                , and I&apos;m keen to seize it!
-            </p> */}
             <p className="mb-3">
                 <span className="italic">
                     An interest in programming and a strong work ethic{' '}
